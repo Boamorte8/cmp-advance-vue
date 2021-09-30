9223dbff0d90bf6fdda38ebbfcaf3a5e56cb6f0d
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2021-09-29 21:45:21 -0500

    Learning about teleport and fragments

--- README.md
+++ README.md
@@ -15,4 +15,10 @@ In this project I will learn about components advanced on Vue
   - You can pass data to the slots to be displayed in the content that define the slot
 - Dynamic Components: when you need to display a component depending on a condition but without to use if statements
   - keep-alive is a tag is a good feature when you need to keep the state of a component even that component is destroyed
-  - 
+- Teleport: is a feature to send a piece of html to other place in the html tree. For example if you have in a component a dialog, you can send it to the body tag
+- Fragments is the feature that allow to components in Vue 3 to have multiples top level o roots tags
+- 
+
+## Differences with Vue 2
+
+- Vue 2 you must to have in each component a root tag. In Vue 3 is not a must
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="closeError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data(){
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      this.inputIsInvalid = false;
+
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      } else {
+        console.log('Set goal')
+      }
+    },
+    closeError() {
+      this.inputIsInvalid = false;
+    },
+  },
+}
+</script>
